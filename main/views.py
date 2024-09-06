7bb36ac1fb19f6e75539c25e0535ac1303961db1
NotChristian <raphaelheryanto270605@gmail.com>
2024-09-07 02:47:23 +0700

    init commit

---
+++ main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+def show_main(request):
+    context = {
+        'npm' : '2306152323',
+        'name': 'Christian Raphael Heryanto',
+        'class': 'PBP D'
+    }
+
+    return render(request, "main.html", context)
